61de7269d6e3c1e402ce78de4dcb317ac012dd93
awroz <awroz.dev@gmail.com>
2023-07-25 20:18:33 -0500

    se comienza con la vinculacion entre la app y webpack

---
+++ src/contadores.js
@@ -0,0 +1,23 @@
+// --------Contador Component -------- //
+function decreaseValue(inputId){
+    const input = document.getElementById(inputId)
+    if(input.value){
+        if(input.value > 0){
+            input.value = parseInt(input.value) - 1
+        }
+    }
+}
+function increaseValue(inputId){
+    const input = document.getElementById(inputId)
+    if(!input.value){input.value = 0}
+    if(input.value >= 0){
+        input.value = parseInt(input.value) + 1
+    }
+}
+
+function hideShowRangos(){
+    const bloque = document.querySelector('#desdeHasta')
+    bloque.classList.toggle('disabled')
+}
+
+export { decreaseValue, increaseValue, hideShowRangos }
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { decreaseValue, increaseValue,  hideShowRangos } from "./contadores"
+
+window.decreaseValue = decreaseValue
+window.increaseValue = increaseValue
