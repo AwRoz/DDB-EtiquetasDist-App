476c7900cccbbbbd71702de762a3beaf9689bd3d
awroz <awroz.dev@gmail.com>
2023-07-27 08:46:01 -0500

    depurado el handler para agregar elementos a la cola, depurados los event listeners

--- src/dynamicComponents.js
+++ src/dynamicComponents.js
@@ -1,3 +1,18 @@
+//Event handler to add a new document to the queue
+function addToQueueHandler(){
+    const colaContainer = document.querySelector('.colaDocumentos')
+    const doc = document.querySelector('#numeroDocumento')
+    const docType = document.querySelector('.docTypeOption input[type="radio"]:checked')
+    if(doc.value && docType){
+        const newDoc = createItemQueue(docType.value,doc.value)
+        colaContainer.appendChild(newDoc)
+        docType.checked = false
+        doc.value = ''
+        doc.focus()
+    }
+}
+
+//function to create a new element li which will be added to the queue
 function createItemQueue(docType,docNumber){
     const newItem = document.createElement('li')
     newItem.classList.add('documentoEnCola')
@@ -7,6 +22,7 @@ function createItemQueue(docType,docNumber){
 
     const newDeleteBtn = document.createElement('span')
     newDeleteBtn.classList.add('deleteItemBtn')
+    newDeleteBtn.setAttribute('onclick','deleteItemBtn(event)')
     newDeleteBtn.innerText = 'Eliminar' 
 
     newItem.appendChild(newTitle)
@@ -15,4 +31,4 @@ function createItemQueue(docType,docNumber){
     return newItem
 }
 
-export { createItemQueue }
+export { addToQueueHandler }
--- src/index.js
+++ src/index.js
@@ -1,47 +1,22 @@
 import { decreaseValue, increaseValue } from './contadores'
-import { createItemQueue } from './dynamicComponents'
+import { addToQueueHandler } from './dynamicComponents'
 
 //Agregar documento a la cola
-const colaContainer = document.querySelector('.colaDocumentos')
-const addToQueueBtn = document.querySelector('#agregaDocumentosCola')
-      addToQueueBtn.addEventListener('click',()=>{
-        const documento = document.querySelector('#numeroDocumento')
-        let tipoDocContainer
-        let tipoDocValue
-        const tipoDocs = document.querySelectorAll('.docTypeContainer input[type="radio"]')
-              tipoDocs.forEach(item => {
-                if(item.checked){
-                  tipoDocValue = item.value
-                  tipoDocContainer = item
-                }
-              })
-        if(documento.value && tipoDocValue){
-            const newDoc = createItemQueue(tipoDocValue, documento.value)
-            colaContainer.appendChild(newDoc)
-          
-            documento.value = ''
-            tipoDocContainer.checked = false
-            documento.focus()
-        }
-      })
-
+document.querySelector('#agregaDocumentosColaBtn')
+.addEventListener('click',addToQueueHandler)
 
 //Manipulacion de contadores
 //--Boton para restar unidades al input
-const decreaseValueBtn = document.querySelectorAll('.decreaseValue')
-      decreaseValueBtn.forEach(btn => {
-        btn.addEventListener('click', decreaseValue)
-      })
+document.querySelectorAll('.decreaseValue')
+.forEach(btn => {btn.addEventListener('click', decreaseValue)})
+
 //--Boton para sumar unidades al input
-const increaseValueBtn = document.querySelectorAll('.increaseValue')
-      increaseValueBtn.forEach(btn => {
-        btn.addEventListener('click', increaseValue)
-      })
+document.querySelectorAll('.increaseValue')
+.forEach(btn => {btn.addEventListener('click', increaseValue)})
 
 
 //Oculta o muestra la personalizacion de rangos
-const rangosBloque = document.querySelector('.desdeHastaTitle')
-      rangosBloque.addEventListener('click',()=>{
+document.querySelector('.desdeHastaTitle')
+.addEventListener('click',()=>{
         document.querySelector('#desdeHasta').classList.toggle('disabled')
       })
-
